264f0a38d3999632f0edeacc8a2398340d63fd2b
Sudip Datta <sudipdatta10@gmail.com>
2017-10-05 12:49:33 +0530

    Add maven pom file and build file for ant for testng

---
+++ src/test/java/com/diskLRUCache/LRUCacheTest.java
@@ -0,0 +1,16 @@
+package com.diskLRUCache;
+
+import org.testng.annotations.*;
+
+public final class LRUCacheTest {
+
+    @BeforeClass
+    public void setup() {
+
+    }
+
+    @Test(groups = {"fast"})
+    public void aFastTest() {
+        System.out.println("Fast Test");
+    }
+}
